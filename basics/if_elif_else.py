a460ff17e3f5f1d809508cbb74b720ca4ad9d0fa
Fayas Noushad <TheFayasNoushad@gmail.com>
2021-10-31 17:53:20 +0530

    Add if elif else statements

---
+++ basics/if_elif_else.py
@@ -0,0 +1,11 @@
+number = 0
+
+if number > 0:
+    text = "This is a negative number"
+elif number < 0:
+    text = "This is a positive number"
+else:
+    text = "Number is zero"
+
+print(text)
+# => Number is zero
